19ec101f510994f19cc7a6453dd8697b073d94dd
edna <edna@LAPTOP-9PK5LSTL>
2022-11-29 11:08:03 +0100

    creating pages

---
+++ nuxt-exercise/pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>This is a about page</h2>
+        <p>
+            After this step instructions vary depending upon whether 
+            you are using Yarn or npm. Yarn is the preferred package 
+            manager for working with Nuxt as it is the development 
+            tool which tests have been written against.
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+
+</style>
